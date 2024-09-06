b9b78a58533f4be8ba0f52bef8dbf74c8aaa31ce
Дима Дубилей <dimon.pr.2007@gmail.com>
2024-09-06 12:31:28 +0300

    Изменил ошибку в правописании

--- ConsoleApplication4/ConsoleApplication4.cpp
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -13,7 +13,7 @@ int main()
     {
         for (int j = 0; j < 3; j++)
         {
-            cout << "Entrer " << sum << " namber: ";
+            cout << "Enter " << sum << " namber: ";
             cin >> a[i][j];
 
             sum++;
