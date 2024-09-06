e823a23c457a55ab4c35df6de16bce7d368d5978
Дима Дубилей <dimon.pr.2007@gmail.com>
2024-09-06 12:27:05 +0300

    Add project files.

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    // masiv 3x3
+    int a[3][3];
+    int sum = 1;
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << "Entrer " << sum << " namber: ";
+            cin >> a[i][j];
+
+            sum++;
+
+        }
+
+    }
+
+    cout << "---------------------------" << endl;
+
+    for (int i = 0; i < 3; i++)
+
+    {
+        for (int j = 0; j < 3; j++)
+
+        {
+
+            cout << a[i][j] << " ";
+
+        }
+
+        cout << endl;
+
+    }
+}
